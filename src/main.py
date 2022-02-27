988bee03ae0d50321637acfb4a43f9bdeb967286
MarcinDudek <dudek.v@gmail.com>
2022-02-27 19:41:07 +0100

    setup aliens

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from re import X
 import pygame, sys
 from player import Player
 import obstacle
+from alien import Alien
 
 
 class Game:
@@ -16,6 +18,21 @@ class Game:
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
         self.create_multiple_obstacles(40, 480, self.obstacle_x_positions)
+
+        # Alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows = 6, cols = 8)
+
+    def alien_setup(self, rows, cols, x_distance = 60, y_distance = 48, x_offset = 70, y_offset = 100):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+
+                if row_index == 0: alien_sprite = Alien('yellow', x, y)
+                elif 1 <= row_index <= 2: alien_sprite = Alien('green', x, y)
+                else: alien_sprite = Alien('red', x, y)
+                self.aliens.add(alien_sprite)
 
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
@@ -40,6 +57,7 @@ class Game:
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
 
 
 if __name__ == '__main__': #TODO: wierd if-main setup
