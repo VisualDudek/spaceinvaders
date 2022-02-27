63d2a418e49e7553717a6e447990dbd4dce393ed
MarcinDudek <dudek.v@gmail.com>
2022-02-27 13:01:03 +0100

    creating game class

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dev process
 1. Create main window
 2. Create the game class
+3. Creating the player
--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 import pygame, sys
 
 
+class Game:
+    def __init__(self) -> None:
+        pass
+        
+    def run(self):
+        # update all spite groups
+        # draw all sprite groups
+        pass
+
+
 if __name__ == '__main__': #TODO: wierd if-main setup
     pygame.init()
     screen_width = 600
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
+    game = Game()
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +26,7 @@ if __name__ == '__main__': #TODO: wierd if-main setup
                 sys.exit()
 
         screen.fill((30,30,30)) #TODO: crate RGB color var
+        game.run()
 
         pygame.display.flip()
         clock.tick(60) #TODO: avoid magic numbers
