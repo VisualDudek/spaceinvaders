0a94859efc34906e6f103dcf5bb58dd369538d9a
MarcinDudek <dudek.v@gmail.com>
2022-02-27 19:03:43 +0100

    placing obstacles

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
 import pygame, sys
 from player import Player
+import obstacle
 
 
 class Game:
     def __init__(self) -> None:
+        #Player seup
         player_sprite = Player((screen_width / 2, screen_height), screen_width, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite) #TODO: co to jest?
+
+        # obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(40, 480, self.obstacle_x_positions)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == 'x':
+                    x = col_index * self.block_size + x_start + offset_x
+                    y = row_index * self.block_size + y_start
+                    block = obstacle.Block(self.block_size, (241,79,80), x, y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self, x_start, y_start, offset):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
         
     def run(self):
         # update all spite groups
@@ -15,6 +38,8 @@ class Game:
 
         self.player.sprite.lasers.draw(screen) # dlaczego poprzez sprite?
         self.player.draw(screen)
+
+        self.blocks.draw(screen)
 
 
 if __name__ == '__main__': #TODO: wierd if-main setup
