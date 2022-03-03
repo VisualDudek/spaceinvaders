f9e7fabe46795d5e7b1a98485de8c2394a610aba
MarcinDudek <dudek.v@gmail.com>
2022-03-03 15:51:39 +0100

    add new SFX

--- src/sound.py
+++ src/sound.py
@@ -2,7 +2,9 @@ import pygame
 
 class Sound():
     def __init__(self):
+        # volume setup
         self.volume = 0.1
+        self.volume_fx = 0.5
 
         self.music = pygame.mixer.Sound('./audio/music.wav')
         self.music.set_volume(self.volume)
@@ -12,6 +14,11 @@ class Sound():
 
         self.explosion_sound = pygame.mixer.Sound('./audio/explosion.wav')
         self.explosion_sound.set_volume(self.volume)
+
+        # SFX
+        self.selectionClick = pygame.mixer.Sound('./audio/selectionClick.wav')
+        self.selectionClick.set_volume(self.volume_fx)
+
 
     def music_up(self):
         self.volume += 0.1
